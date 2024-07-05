38c8f19ebdb8b6394a96e449b17c4778a1ba078e
charlessamsonccs <149385341+charlessamsonccs@users.noreply.github.com>
2024-07-05 11:49:32 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("this is sampson web page");
+let p=prompt("what is your name")
+console.log(p);
+document.querySelector(".ph").textContent=p;
+
+let hour= 17;
+if (hour < 18) {
+    greeting = "good day";
+console.log(greeting)
+}
